768912e9814b5d3793bd1390e7f565af747945a3
shootinguy <sokotreston@gmail.com>
2025-03-26 14:52:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log(3 > 5);
+console.log(typeof true);
+console.log(typeof 'false');
+console.log(5 === '5.00');
+
+const age = 15
+
+if(15 >= 16) {
+    console.log('You can drive');
+} else {
+    console.log('You can not drive');
+}
+
+if(15 >= 16) {
+    console.log('Almost there');
+} else if (age >= 14) {
+    console.log('You can not drive');
+}
+
+const age = 8 
+
+if (time < 12) {
+    console.log("Good Morning")
+} else if (time === 12) {
+    console.log("Wait it is noon")
+} else if (time > 12 && time < 18) {
+    console.log("Good Afternoon")
+}  else {
+    console.log('Good evening');
+}
+
+let weather = 'rain'
+if (weather == 'rain') {
+    console.log('Grab your umbrella')
+} else {
+    console.log('Wear your sunglasses')
+}
+
+let weather = prompt('How is the weather?')
+if (weather == 'rain') {
+    console.log('Grab your umbrella')
+} else {
+    console.log('Wear your sunglasses')
+}
+
